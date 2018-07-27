8027cdbce15b434a197416a76b08de75f424999e
William Bork <williambork33@gmail.com>
2018-07-27 01:58:52 -0500

    Connecting server.js and config.js to database.

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-require("dotenv").config();
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+
+// Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
+// Set the port of our application
 var PORT = process.env.PORT || 3000;
+
+// Requiring Models
+var db = require("./models");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,13 +27,13 @@ app.use(express.static("public"));
 // Handlebars
 app.engine(
   "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+  exphbs({defaultLayout: "main"})
 );
+
 app.set("view engine", "handlebars");
 
 // Routes
+// =============================================================
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
@@ -34,15 +45,12 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Syncing sequelize models, start Express app, begin listening to client requests
+// ============================================================= 
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    console.log("App listening on PORT " + PORT);
+    });
 });
 
 module.exports = app;
