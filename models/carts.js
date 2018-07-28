d7dc76b63ef9db33b511f5284e7c8ba864dce746
Jorge Delgado <jorge7delgado@gmail.com>
2018-07-28 14:00:16 -0500

    data base, models, and html routes to show in main page

--- models/carts.js
+++ models/carts.js
@@ -1,18 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Products = sequelize.define('Products', {
+  var Carts = sequelize.define('Carts', {
     item_id: DataTypes.STRING,
     brand: DataTypes.STRING,
     type: DataTypes.STRING,
     size: DataTypes.STRING,
     quantity: DataTypes.INTEGER,
-    cost: DataTypes.DECIMAL,
     regular_price: DataTypes.DECIMAL,
-    discount: DataTypes.INTEGER,
-    pre_order: DataTypes.INTEGER
+    discount: DataTypes.DECIMAL,
+    quantity: DataTypes.INTEGER,
+    total_price: DataTypes.DECIMAL,
+    customer_name: DataTypes.STRING
   }, {});
-  Products.associate = function(models) {
+  Carts.associate = function(models) {
     // associations can be defined here
   };
-  return Products;
+  return Carts;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,61 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+
+
+  //HTML route to show all collections
+  app.get("/collections", function (req, res) {
+    db.Products.findAll({}).then(function (collections) {
+      res.json(collections);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //PENDIENTE HACER LOS DE POST Y UPDATE ya que eso es del admin y ahorita para que podamos jalar los collections para mostrar
+
+
+  //HTML route for types of product id
+  app.get("/collections/:id", function (req, res) {
+    db.Products.findAll({ where: { item_id: req.params.type } }).then(function (typecollections) {
+      res.json(typecollections);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  //HTML route for types of collections
+  app.get("/collections/:type", function (req, res) {
+    db.Products.findAll({ where: { type: req.params.type } }).then(function (typecollections) {
+      res.json(typecollections);
     });
   });
+
+  //HTML route for Sales
+  app.get("/collections/Sales", function (req, res) {
+    db.Products.findAll({ where: { discount: 1 } }).then(function (typecollections) {
+      res.json(typecollections);
+    });
+  });
+
+  //HTML route for Sales and types
+  app.get("/collections/Sales/:type", function (req, res) {
+    db.Products.findAll({ where: { discount: 1, type: req.params.type } }).then(function (typecollections) {
+      res.json(typecollections);
+    });
+  });
+
+  //HTML route for Preorder
+  app.get("/collections/Preorder", function (req, res) {
+    db.Products.findAll({ where: { pre_order: 1 } }).then(function (typecollections) {
+      res.json(typecollections);
+    });
+  });
+
+
+  //HTML route for Preorder and types
+  app.get("/collections/Preorder/:type", function (req, res) {
+    db.Products.findAll({ where: { pre_order: 1, type: req.params.type } }).then(function (typecollections) {
+      res.json(typecollections);
+    });
+  });
+
+
+
 };
